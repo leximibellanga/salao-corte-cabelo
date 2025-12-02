a358e521c0f3c13d0eb5a80c5410e3fce20d499f
Leximíbel Langa <langaleximibel@gmail.com>
2025-12-02 16:40:00 +0200

    actualizacao

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ botaoFecharMenu.addEventListener('click', () => {
 
 
 
+
+/* =============== BOTAO DE 'VER MAIS' IMAGENS DO PORTFOLIO ================*/
+let verMais = document.getElementById('ver-mais')
+let portfolioImg = document.getElementsByClassName('portfolio-img')
+
+verMais.addEventListener('click', () => {
+    if (verMais.textContent == 'ver mais') {
+        for(let i = 6; i < portfolioImg.length; i++) {
+            portfolioImg[i].style.display = 'block'
+        }
+        verMais.textContent = 'ver menos'
+    } else {
+        for(let i = 6; i < portfolioImg.length; i++) {
+            portfolioImg[i].style.display = 'none'
+        }
+        verMais.textContent = 'ver mais'
+    }
+})
+
+/* =============== BOTAO PARA ABRIR FORMULARIO DE AGENDAMENTO ================*/
+let botaoAgendar = document.getElementsByClassName('botao-agendar')
+let agendar = document.getElementById('agendar') 
+
+for (let i = 0; i < botaoAgendar.length; i++) {
+    botaoAgendar[i].addEventListener('click', () => {
+        agendar.style.display = 'block'
+    })
+}
